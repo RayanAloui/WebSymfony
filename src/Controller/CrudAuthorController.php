b630362a92bdef9f7024788b2a1ae19c8196a4a1
RayanAloui <ahmedrayen.aloui@esprit.tn>
2024-10-07 00:16:40 +0100

    1ère séance Doctrine

---
+++ src/Controller/CrudAuthorController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\AuthorRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/crud/author')]
+class CrudAuthorController extends AbstractController
+{
+    #[Route('/list', name: 'app_crud_author')]
+    public function list(AuthorRepository $repository): Response
+    {
+        //Récupération des données 
+        $list=$repository->findAll();
+        return $this->render('crud_author/list.html.twig',
+        ['list' =>$list]);
+    }
+}
